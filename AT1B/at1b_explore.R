389eb8c401866e09f11fb8dcdcb691266dd3677c
Mutaz Abu Ghazaleh <mutaza@microsoft.com>
2020-09-04 13:51:18 +1000

    added plots with confidence intervals for pnl

--- AT1B/at1b_explore.R
+++ AT1B/at1b_explore.R
@@ -4,9 +4,60 @@ source('at1b_13184383_final.R')
 
 year_court = 2
 exploration_plan = data.frame(year = c(1,2,3,4,5),
-                              teams = c(1,2,2,2,2))
+                              teams = c(3,2,2,2,1))
 
 sims1 <- whatif(year_court, exploration_plan)
+
+
+
+as.data.frame(sims1$pnl) %>% head() %>% summarise_all(list(m1 = min, m2=max))
+
+
+as.data.frame(sims1$pnl) %>% head() %>% pivot_longer(cols = year1:year5) %>% 
+  group_by(name) %>% 
+  summarise(.funs=quantile, c(.1,.5,.9))
+
+
+as.data.frame(sims1$pnl) %>% 
+  summarise(across(year1:year5, quantile, c(.025, .05,.1,.5,.9,.95,.975))) %>% 
+  'rownames<-' (c("0:2.5%", "1:5%","2:10%","3:50%","4:90%", "5:95%","6:97.5%")) %>% 
+  rownames_to_column('quantiles') %>% 
+  pivot_longer(cols=year1:year5) %>% 
+  ggplot(aes(x=name, y=value, group=quantiles, colour=quantiles)) +
+  geom_line() + 
+  geom_point() + 
+  scale_y_continuous(labels = scales::comma)
+
+
+
+as.data.frame(sims1$pnl) %>% 
+  summarise(across(year1:year5, quantile, 1-c(.025, .05,.1,.5,.9,.95,.975))) %>% 
+  'rownames<-' (c("0:2.5%", "1:5%","2:10%","3:50%","4:90%", "5:95%","6:97.5%")) %>% 
+  rownames_to_column('quantiles') %>% 
+  pivot_longer(cols=year1:year5) %>% 
+  ggplot(aes(x=name, y=value, group=quantiles, colour=quantiles)) +
+  geom_line() + 
+  geom_point() + 
+  scale_y_continuous(labels = scales::comma)
+
+  print_quantiles('final position', sims1$final_position)
+  
+  # rownames_to_column(as.character(c(.025, .05,.1,.5,.9,.95,.975)))
+
+
+as.data.frame(sims1$pnl) %>% head() %>% 
+  summarise(across(year1:year5,ecdf))
+
+
+quantile((as.data.frame(sims1$pnl) %>% head())$year1)
+
+y1 <- (as.data.frame(sims1$pnl) %>% head())$year1
+y1
+median(y1)
+
+quantile(y1, c(.5))
+
+
 claim_proceeds_budget_fx <- ecdf(sims$claim_proceeds$year1 + sims$budget)
 plot(claim_proceeds_budget_fx)
 
