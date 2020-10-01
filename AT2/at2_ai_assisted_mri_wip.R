06851ecb2afd95a9b75903bbb2ce64e5126102a6
Mutaz Abu Ghazaleh <mutaza@microsoft.com>
2020-10-01 16:33:12 +1000

    added params file, simulation function and simulation plotting function

---
+++ AT2/at2_ai_assisted_mri_wip.R
@@ -0,0 +1,109 @@
+# AT2: Business Case Development
+# AI-Assisted Accelerated MRI 
+
+# Mutaz Abu Ghazaleh: 13184383 
+# Jay Radhakrishnan: 13037686
+# Anand KC: 13386331
+# Andrew Barbour: 12822589
+# Chris Garces: 12473310
+
+
+
+library(tidyverse)
+library(gridExtra)
+
+
+
+
+#### functions definitions ####
+
+inv_triangle_cdf <- function (P, vmin, vml, vmax){
+  # inverse triangle cdf is used to translate probabilities based on triangular
+  # PDF, stuiable for 3-point estimate simulation
+  Pvml <- (vml-vmin)/(vmax-vmin)
+  
+  return(ifelse(P < Pvml,
+                vmin + sqrt(P*(vml-vmin)*(vmax-vmin)),
+                vmax - sqrt((1-P)*(vmax-vml)*(vmax-vmin))))
+}
+
+
+get_param <- function(category='trial', parameter='fda_approval_time', param_file = './parameters.csv'){
+  if (!exists('sim_params')){
+    print('loading sim params')
+    sp <- read.csv(param_file,stringsAsFactors = FALSE)
+    assign('sim_params', sp, envir = .GlobalEnv)
+  }
+  
+  p <- sim_params %>% filter(cat == category, param == parameter)
+  
+  switch (p$type,
+          '1-point' = p <- p %>% select(vmin) %>% first(),
+          'prob' = p <- p %>% select(vmin) %>% first(),
+          '3-point' = p <- p %>% select(vmin, vml, vmax)
+  )
+  
+  return(p)
+}
+
+sim <- function(category='trial',
+                parameter='fda_approval_time',
+                n_trials = 10000,
+                reps = FALSE){
+  
+  v <- get_param(category, parameter)
+  
+  ret <- rep(ifelse(reps, v, 0), n_trials)
+  
+  if (is.data.frame(v)){
+    ret <- inv_triangle_cdf(
+      P = runif(n_trials),
+      vmin = v$vmin,
+      vml = v$vml,
+      vmax = v$vmax
+    )
+  } else if (is.numeric(v) && !reps){
+    ret <- rbinom(n=n_trials, size=1, prob=v)
+  }
+  
+  return(ret)
+}
+
+
+
+
+
+print_quantiles <- function(label, series){ 
+  fx <- ecdf(series)
+  
+  qq <- round(quantile(fx, c(.05,.5,.95), type=7)/1e+6, 2)
+  
+  print(paste0('   ', label, ' in $mil 95% over: ', qq[[1]], ', 50% over: ', qq[[2]], ', 5% over: ', qq[[3]]))
+}
+#### plotting functions ####
+
+plot_sim_values <- function(s){
+  qq = quantile(ecdf(s),c(0.05,0.5,0.9,.95,1),type=7)
+  Fx <- ecdf(s)
+  hist(s, breaks=50, probability = TRUE, 
+       main = "Project Duration PDF", xlab="estimated duration",
+       xlim=c(19,40))
+  lines(density(s), lwd=2)
+  plot(ecdf(s), main = "Project Duration CDF",ylab = "likelihood", xlab="estimated duration")
+  x = qq['95%']
+  y = .95
+  abline(v=x, h=y, col = "lightgray")      
+  points(x,y,  col = "red", pch=20)
+  text(x=x,y=y, labels = paste('(',round(x,2),',',round(y,2)*100,'%)'))
+  x = qq['50%']
+  y = .5
+  abline(v=x, h=y, col = "lightgray")      
+  points(x,y,  col = "blue", pch=20)
+  text(x=x,y=y, labels = paste('(',round(x,2),',',round(y,2)*100,'%)'))
+  x = qq['5%']
+  y = .05
+  abline(v=x, h=y, col = "lightgray")      
+  points(x,y,  col = "red", pch=20)
+  text(x=x,y=y, labels = paste('(',round(x,2),',',round(y,2)*100,'%)'))
+  
+}
